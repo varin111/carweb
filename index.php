b9196840d4c55ea6c7af6179e91fee47b18d09f5
Muhammad Esmail <ihama9728@gmail.com>
2025-02-09 00:16:08 +0300

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/config/front/header.php';
 ?>
 <main class="main">
-    <section id="hero" class="hero section dark-background" >
+    <section id="hero" class="hero section dark-background">
         <div class="container">
             <div class="row gy-4">
                 <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
@@ -238,7 +238,7 @@ require_once __DIR__ . '/config/front/header.php';
                                 </p>
                             </div>
                         </div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 270px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3219.553010919305!2d43.96920137547042!3d36.20175127242381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x400723862d9c2b5f%3A0xa9a44163b9df376c!2sEmpire%20World!5e0!3m2!1sen!2siq!4v1739049167121!5m2!1sen!2siq" frameborder="0" style="border:0; width: 100%; height: 270px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
                 <div class="col-lg-7">
