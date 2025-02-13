0eebf3fe5fc80f73fb40c4557940f63a7aef3642
varin11 <varinkamil666@gmail.com>
2025-02-13 21:47:04 +0300

    update

--- config/vars.php
+++ config/vars.php
@@ -3,14 +3,14 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'var_car_insurance');
+define('DB_NAME', 'car_insurance');
 
 
-define('SITE_URL', 'http://var_php_car_insurance.test/');
+define('SITE_URL', 'http://localhost/carwebmainn/');
 define('SITE_NAME', 'Kurd Car Insurance');
 define('SITE_EMAIL', 'muhammad@gmail.com');
 define('SITE_PHONE', '1234567890');
-define('SITE_ADDRESS', '123, Main Street, New York, USA');
+define('SITE_ADDRESS', '123, Main Street, erbil, empire');
 define('PATH_IMAGE_UPLOAD', 'assets/uploads/');
 define('PATH_IMAGE_UPLOAD_VEHICLES', 'assets/uploads/vehicles/');
 
